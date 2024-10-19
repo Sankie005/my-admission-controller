d7ac3d9e3f812ed7c499c79a7c3a721bbc71ab5d
Saarai <saarai@ucsc.edu>
2024-10-19 12:28:17 -0700

    downgrade to http to fix TLS issue

--- main.go
+++ main.go
@@ -1,41 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/sankie005/my-admission-controller/pkg/webhook"
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
-	webhookruntime "sigs.k8s.io/controller-runtime/pkg/webhook"
+	"github.com/sankie005/my-admission-controller/pkg/webhook" // Update this to match the module path in your go.mod
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 func main() {
-	// Get a config to talk to the apiserver
-	cfg, err := config.GetConfig()
+	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Port: 80,
+	})
 	if err != nil {
-		fmt.Printf("Error getting config: %v\n", err)
 		os.Exit(1)
 	}
 
-	// Create a new Cmd to provide shared dependencies and start components
-	mgr, err := manager.New(cfg, manager.Options{})
-	if err != nil {
-		fmt.Printf("Error creating manager: %v\n", err)
-		os.Exit(1)
-	}
+	wh := &webhook.AdmissionController{}
 
-	// Setup webhooks
-	fmt.Println("Setting up webhook server")
-	hookServer := mgr.GetWebhookServer()
+	mgr.GetWebhookServer().Register("/validate-jobs", wh)
 
-	fmt.Println("Registering webhooks to the webhook server")
-	hookServer.Register("/validate", &webhookruntime.Admission{Handler: &webhook.AdmissionController{}})
-
-	fmt.Println("Starting manager")
-	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
-		fmt.Printf("Error starting manager: %v\n", err)
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		os.Exit(1)
 	}
 }
