1b7e8494f436858729c5f115a13ad55f6914b6ca
Saarai <saarai@ucsc.edu>
2024-10-19 12:48:12 -0700

    removed unecessary files

--- run.sh
+++
@@ -1,7 +0,0 @@
-chmod +x *.sh
-echo "Deploying Admission Controller"
-./deploy-admission-controller.sh
-echo "Generating TLS certifications"
-./generate-certs.sh
-echo "Testing Admission Controller"
-./test-admission-controller.sh
